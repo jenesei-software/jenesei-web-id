c4d432d068d895613eff026cbf6b9b7a7b5b05ce
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-09-09 02:25:58 +0700

    Update.

--- src/pages/user-profile/user-profile.tsx
+++ src/pages/user-profile/user-profile.tsx
@@ -1,5 +1,17 @@
-import { Button, Icon, Stack, Typography, useAppContext } from '@jenesei-software/jenesei-ui-react'
-import { queryKeys, useGetResourceAll, useGetSSOLogout, useGetSSOProfile } from '@jenesei-software/jenesei-web-id-api'
+import {
+  Button,
+  ModuleResource,
+  ModuleSession,
+  Stack,
+  Typography,
+  useAppContext,
+} from '@jenesei-software/jenesei-ui-react'
+import {
+  queryKeys,
+  useDeleteSessionTerminate,
+  useGetSSOLogout,
+  useGetSSOProfile,
+} from '@jenesei-software/jenesei-web-id-api'
 import { useEffect } from 'react'
 
 import { queryClient } from '@core/query'
@@ -7,7 +19,6 @@ import { queryClient } from '@core/query'
 export function UserProfile() {
   const { changeTitle, historyTitle } = useAppContext()
   const { data: dataProfile } = useGetSSOProfile({ retry: false })
-  const { data: dataResourceAll } = useGetResourceAll({ retry: false })
 
   const { mutate: mutateGetSSOLogout } = useGetSSOLogout({
     onSuccess: () => {
@@ -22,26 +33,34 @@ export function UserProfile() {
     },
   })
 
+  const { mutate: mutateDeleteSessionTerminate } = useDeleteSessionTerminate({
+    onSuccess: () => {
+      Promise.all([
+        queryClient.invalidateQueries({
+          queryKey: [queryKeys.sso.profile],
+        }),
+        queryClient.invalidateQueries({
+          queryKey: [queryKeys.session.all],
+        }),
+      ])
+    },
+    onError: () => {
+      console.log('error')
+    },
+  })
+
   useEffect(() => {
     if (dataProfile?.nickname) changeTitle(dataProfile?.nickname)
     return () => historyTitle(0)
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [dataProfile])
   return (
-    <Stack gap="10px" alignItems="center" flexDirection="column">
+    <Stack maxW="600px" w="100%" gap="50px" alignItems="stretch" flexDirection="column">
       <Typography variant="h5">{dataProfile?.nickname.toString()}</Typography>
-      <Stack flexDirection="column" alignItems="flex-start" gap="16px">
-        {dataResourceAll?.map((e) => (
-          <Stack flexDirection="row" alignItems="center" justifyContent="space-around" gap="10px">
-            <Typography>{e.resourceName}</Typography>
-            {e.isActive ? (
-              <Icon type="curved" size="medium" primaryColor="green100" name="TickSquare" />
-            ) : (
-              <Icon type="curved" size="medium" primaryColor="black40" name="ArrowRightCircle" />
-            )}
-          </Stack>
-        ))}
-      </Stack>
+      <ModuleResource />
+      <ModuleSession
+        onDeleteSession={(sessionId) => mutateDeleteSessionTerminate({ path: { sessionId: sessionId } })}
+      />
       <Button onClick={mutateGetSSOLogout} genre={'gray'} size={'small'}>
         Logout
       </Button>
