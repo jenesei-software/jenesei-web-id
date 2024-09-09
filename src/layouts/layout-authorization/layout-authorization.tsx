3f8e2a63598c9eef7abe76c1213a5548d2f98a1e
Cyril Strone <77802640+CyrilStrone@users.noreply.github.com>
2024-09-09 08:57:08 +0700

    Update.

--- src/layouts/layout-authorization/layout-authorization.tsx
+++ src/layouts/layout-authorization/layout-authorization.tsx
@@ -1,8 +1,14 @@
+import { useAppContext } from '@jenesei-software/jenesei-ui-react'
 import { Navigate, Outlet } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 import { LayoutAuthorizationWrapper } from '.'
 
 export function LayoutAuthorization() {
+  const { changePreview } = useAppContext()
+  useEffect(() => {
+    changePreview({ isShow: false })
+  }, [changePreview])
   return (
     <LayoutAuthorizationWrapper>
       <Outlet />
--- src/layouts/layout-user/layout-user.tsx
+++ src/layouts/layout-user/layout-user.tsx
@@ -1,8 +1,14 @@
+import { useAppContext } from '@jenesei-software/jenesei-ui-react'
 import { Navigate, Outlet } from '@tanstack/react-router'
+import { useEffect } from 'react'
 
 import { LayoutUserWrapper } from '.'
 
 export function LayoutUser() {
+  const { changePreview } = useAppContext()
+  useEffect(() => {
+    changePreview({ isShow: false })
+  }, [changePreview])
   return (
     <LayoutUserWrapper>
       <Outlet />
